f2773b489ddef2219f318c479804056113fc6d29
Semjon Geist <semjon.geist@ionos.com>
2022-09-27 15:50:13 +0200

    remove digest from cpp functions

--- cornflakes/__init__.py
+++ cornflakes/__init__.py
@@ -16,13 +16,13 @@ __________________________________
     apply_match
 """  # noqa: RST303 D205
 
-__author__ = "Semjon Geist"
-__email__ = "semjon.geist@ionos.com"
-__version__ = "1.4.4"
-
 from _cornflakes import apply_match, eval_csv, eval_datetime, eval_type, extract_between, ini_load
 from cornflakes._default_ca_path import default_ca_path
 from cornflakes._types import DatetimeMS, datetime_ms
+
+__author__ = "Semjon Geist"
+__email__ = "semjon.geist@ionos.com"
+__version__ = "1.4.4"
 
 # static definition for reference to
 # ini_load = None
@@ -64,6 +64,4 @@ __all__ = [
     "eval_csv",
     "extract_between",
     "apply_match",
-    "simple_hmac",
-    "simple_sha256",
 ]
