7bae7f9aa459fd98e64049a2ec1b1e3e22b78818
Semjon Geist <semjon.geist@ionos.com>
2022-10-06 15:42:33 +0200

    try fix isort different behaviour in ci and local

--- cornflakes/__init__.py
+++ cornflakes/__init__.py
@@ -15,7 +15,6 @@ __________________________________
     extract_between
     apply_match
 """  # noqa: RST303 D205
-
 from _cornflakes import apply_match, eval_csv, eval_datetime, eval_type, extract_between, ini_load
 from cornflakes._default_ca_path import default_ca_path
 from cornflakes._types import DatetimeMS, datetime_ms
@@ -35,32 +34,3 @@ __all__ = [
     "extract_between",
     "apply_match",
 ]
-
-# static definition for reference to
-# ini_load = None
-# eval_type = None
-# eval_datetime = None
-# eval_csv = None
-# extract_between = None
-# apply_match = None
-# simple_hmac = None
-# simple_sha256 = None
-
-# dynamic load of pybind11 modules to create better docs
-# pybind11_modules = [str(key) for key in _cornflakes.__dict__.keys() if re.match("[a-z].*", key)]
-# for func_name in pybind11_modules:
-#     _func = _cornflakes.__dict__.get(func_name)
-#     func = None
-#     ref_str = f"_cornflakes.{func_name}"
-#     docs = _func.__doc__.split("\n")
-#     args_str = ",".join(
-#         [value.split(":")[0] for value in _func.__doc__.split("\n")[0].split("(")[1].split(")")[0].split(",")]
-#     )
-#     exec(  # noqa: S102
-#         f"""
-# def {docs[0]}:
-#      '''reference of :meth:`{ref_str}`'''
-#      return _cornflakes.__dict__.get("{func_name}")({args_str})
-#     """,
-#         locals(),
-#     )
