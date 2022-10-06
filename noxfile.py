593cb21c1317a3b9fa1e7a542e09706597206dc6
Semjon Geist <semjon.geist@ionos.com>
2022-10-06 19:54:58 +0200

    fix nox tests build docs requirements

--- noxfile.py
+++ noxfile.py
@@ -216,7 +216,7 @@ def docs_build(session: Session) -> None:
     search = f"*cp{version}*.whl"
     file = list(Path("dist").glob(search))[0].name
     session.run("pip", "install", f"dist/{file}")
-    session.install("sphinx", "sphinx-click", "sphinx-rtd-theme", "sphinx-rtd-dark-mode")
+    session.install("sphinx", "sphinx-click", "sphinx-rtd-theme", "sphinx-rtd-dark-mode", "myst-parser", "breathe")
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
@@ -235,8 +235,15 @@ def docs(session: Session) -> None:
     search = f"*cp{version}*.whl"
     file = list(Path("dist").glob(search))[0].name
     session.run("pip", "install", f"dist/{file}")
-    session.install("myst-parser", "breathe")
-    session.install("sphinx", "sphinx-autobuild", "sphinx-click", "sphinx-rtd-theme", "sphinx-rtd-dark-mode")
+    session.install(
+        "sphinx",
+        "sphinx-autobuild",
+        "sphinx-click",
+        "sphinx-rtd-theme",
+        "sphinx-rtd-dark-mode",
+        "myst-parser",
+        "breathe",
+    )
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
