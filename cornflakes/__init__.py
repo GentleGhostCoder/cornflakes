e5dda97dd9498c836756d40d0bb68f44c4c94d95
Semjon Geist <semjon.geist@ionos.com>
2022-10-13 04:22:44 +0200

    Bump version from 2.1.1 to 2.1.2

--- cornflakes/__init__.py
+++ cornflakes/__init__.py
@@ -22,7 +22,7 @@ from cornflakes._add_dataclass_slots import add_slots
 
 __author__ = "Semjon Geist"
 __email__ = "semjon.geist@ionos.com"
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 
 __all__ = [
     "datetime_ms",
