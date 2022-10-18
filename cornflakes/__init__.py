e59e6bb5d85796e6cc3a93e851df23341b4ac070
Semjon Geist <semjon.geist@ionos.com>
2022-10-18 11:53:48 +0200

    Bump version from 2.4.0 to 2.4.1

--- cornflakes/__init__.py
+++ cornflakes/__init__.py
@@ -27,7 +27,7 @@ from cornflakes.decorator import ini_config
 
 __author__ = "Semjon Geist"
 __email__ = "semjon.geist@ionos.com"
-__version__ = "2.4.0"
+__version__ = "2.4.1"
 
 __all__ = [
     "datetime_ms",
