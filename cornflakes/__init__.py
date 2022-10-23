200ee4262890e78c5a7d3d8f7055651a6857c290
Semjon Geist <semjon.geist@ionos.com>
2022-10-23 14:01:15 +0200

    Bump version from 2.6.2 to 2.6.3

--- cornflakes/__init__.py
+++ cornflakes/__init__.py
@@ -27,7 +27,7 @@ from cornflakes.parser import yaml_load
 
 __author__ = "Semjon Geist"
 __email__ = "semjon.geist@ionos.com"
-__version__ = "2.6.2"
+__version__ = "2.6.3"
 
 __all__ = [
     "ini_load",
