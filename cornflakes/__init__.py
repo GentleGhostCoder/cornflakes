0868ff76c4ce074a031f4963e37728e7f7457a0d
Semjon Geist <semjon.geist@ionos.com>
2022-10-26 21:15:07 +0200

    Bump version from 2.8.0 to 2.8.1

--- cornflakes/__init__.py
+++ cornflakes/__init__.py
@@ -26,7 +26,7 @@ from cornflakes.parser import yaml_load
 
 __author__ = "Semjon Geist"
 __email__ = "semjon.geist@ionos.com"
-__version__ = "2.8.0"
+__version__ = "2.8.1"
 
 __all__ = [
     "click_cli",
