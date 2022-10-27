a6cbe49b41ee33449db9fcd698e37bc7b3b7979a
Semjon Geist <semjon.geist@ionos.com>
2022-10-27 20:37:45 +0200

    fix auto_option

--- cornflakes/click/options/_auto_option.py
+++ cornflakes/click/options/_auto_option.py
@@ -5,7 +5,7 @@ from typing import Any, Callable, Dict, List, TypeVar, Union
 from click import Command, Group, option
 
 from cornflakes.click import argument
-from cornflakes.decorator.config import Config, ConfigGroup
+from cornflakes.decorator.config import Config, ConfigGroup, is_config
 
 F = TypeVar(
     "F",
@@ -13,7 +13,7 @@ F = TypeVar(
 )
 
 
-def auto_option(config: Union[Config, ConfigGroup], **options) -> F:
+def auto_option(config: Union[Config, ConfigGroup], **options) -> F:  # noqa: C901
     """Click Option Decorator to define a global option for cli decorator."""
     if not isclass(config):
         raise TypeError("config should be a class!")
@@ -33,11 +33,17 @@ def auto_option(config: Union[Config, ConfigGroup], **options) -> F:
         for slot_name in config.__slots__:
             callback = option(f"--{slot_name.replace('_', '-')}", help=configs.get(slot_name, ""))(callback)
 
-        callback = argument("Section-Name", type=str, required=False, nargs=1)(callback)
+        callback = argument("File Name", type=str, required=False, nargs=1)(callback)
+        if is_config(config):
+            callback = argument("Section Name", type=str, required=False, nargs=1)(callback)
 
         @wraps(callback)
-        def wrapper(**kwargs):
-            __config: Union[Dict[str, Union[Config, List[Config]]], ConfigGroup] = config.from_file(**kwargs)
+        def wrapper(*args, **kwargs):
+            __config: Union[Dict[str, Union[Config, List[Config]]], ConfigGroup] = config.from_file(*args, **kwargs)
+            if len(args) == 2:
+                __config = __config.popitem()[1]
+                if isinstance(__config, list):
+                    __config = __config[1]
             return callback(config=__config)
 
         return wrapper
