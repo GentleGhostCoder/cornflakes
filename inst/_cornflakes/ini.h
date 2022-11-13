b8f288e2ea451a477d4e18d4ef664a8dab74770e
Semjon Geist <semjon.geist@ionos.com>
2022-11-13 04:18:47 -0500

    change section end pattern

--- inst/_cornflakes/ini.h
+++ inst/_cornflakes/ini.h
@@ -18,8 +18,7 @@ inline const char NEWVALUE = '=';
 inline const char WHITESPACE = ' ';
 inline const std::string SECTION_OPEN_CHAR = "[";
 inline const std::string SECTION_CLOSE_CHAR = "]";
-inline const std::string BEGIN_PATTERN =
-    system_operations::LINE_SEPERATOR + SECTION_OPEN_CHAR;
+inline const std::string BEGIN_PATTERN = '\n' + SECTION_OPEN_CHAR;
 py::dict ini_load(
     const std::map<std::string, std::vector<std::string>> &files,
     const std::map<std::string, std::vector<std::string>> &sections,
