1fd124e44c023447ceaa44ae4286440f2044722e
Semjon Geist <semjon.geist@ionos.com>
2023-01-03 12:55:20 +0100

    fix tests

--- cornflakes/logging/__init__.py
+++ cornflakes/logging/__init__.py
@@ -2,7 +2,7 @@
 __________________________________
 """  # noqa: RST303 D205
 from cornflakes.common import patch_module
-from cornflakes.logging._logger import attach_log, setup_logging
+from cornflakes.logging.logger import attach_log, setup_logging
 
 __all__ = ["attach_log", "setup_logging"]
 
