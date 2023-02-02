acd38e2d5155104614d84ad16e01630442d3ed46
Semjon Geist <semjon.geist@ionos.com>
2023-02-02 22:25:11 +0100

    add manylinux build

--- tests/test_speed.py
+++ tests/test_speed.py
@@ -10,14 +10,14 @@ import cornflakes
 class TestSpeed(unittest.TestCase):
     """Tests for eval_datetime."""
 
-    @pytest.mark.skipif(os.environ.get("NOX_RUNNING", False))
+    @pytest.mark.skipif(os.environ.get("NOX_RUNNING", "False"))
     def test_ini_load_speed(self):
         s = perf_counter()
         for _ in range(1000):
             cornflakes.ini_load("tests/configs/default.ini")
         self.assertTrue(0.11 > (perf_counter() - s))
 
-    @pytest.mark.skipif(os.environ.get("NOX_RUNNING", False))
+    @pytest.mark.skipif(os.environ.get("NOX_RUNNING", "False"))
     def test_eval_csv_speed(self):
         s = perf_counter()
         with open("tests/smallwikipedia.csv", "rb") as f:
