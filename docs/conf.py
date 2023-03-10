35bd3c1a4066862d122a5e469e9d8510bd4b29e6
Semjon Geist <semjon.geist@ionos.com>
2023-03-10 21:16:51 +0100

    remove sphinxcontrib-inlinesyntaxhighlight

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ extensions = [
     "sphinx_click",
     "sphinx_rtd_dark_mode",
     "sphinx.ext.autosummary",
-    "sphinxcontrib.inlinesyntaxhighlight",
+    # "sphinxcontrib.inlinesyntaxhighlight",
     "myst_parser",
     "breathe",
 ]
