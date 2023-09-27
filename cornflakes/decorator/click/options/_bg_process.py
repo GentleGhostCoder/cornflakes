312c0edf761a6d2de072ae0bddf28101e7ac61e3
Semjon Geist <semjon.geist@ionos.com>
2023-09-27 18:05:52 +0200

    fix background-process

--- cornflakes/decorator/click/options/_bg_process.py
+++ cornflakes/decorator/click/options/_bg_process.py
@@ -1,10 +1,9 @@
-from inspect import getfile
 import logging
-from os.path import abspath
 import subprocess  # noqa: S404
 import sys
 from typing import Any, Union
 
+from cornflakes.decorator.click.helper import get_command_name
 from cornflakes.decorator.click.options._global import global_option
 from cornflakes.decorator.click.rich import RichCommand, RichGroup
 
@@ -14,14 +13,15 @@ logger = logging.getLogger(__name__)
 @global_option(
     ["-b", "--background-process"],
     is_flag=True,
-    # option_group="Basic Options",
     help="Run in Background without console logger.",
 )
-def bg_process_option(self: Union[RichCommand, RichGroup, Any], background_process: bool, *func_args, **func_kwargs):
+def bg_process_option(self: Union[RichCommand, RichGroup, Any], background_process: bool):
     """Default Option for running in background."""
     if background_process:
-        stdout_file = f"{self.callback.__name__}.log"
-        stderr_file = f"{self.callback.__name__}_error.log"
+        print(self)
+        command_name = get_command_name(self).replace(" ", "_").replace("-", "_")
+        stdout_file = f"{command_name}.log"
+        stderr_file = f"{command_name}_error.log"
         logger.debug(
             f"Method {self.callback.__name__} is running in background. "
             f"See logs at stdout: {stdout_file}, stderr: {stderr_file}."
@@ -29,20 +29,13 @@ def bg_process_option(self: Union[RichCommand, RichGroup, Any], background_proce
         stdout = open(stdout_file, "w")
         stderr = open(stderr_file, "w")
 
-        command = (
-            f"import sys; exec(open("
-            f"{abspath(getfile(self.callback))!r}).read());"
-            f"{self.callback.__name__}(*{func_args},**{func_kwargs});"
-        )
+        # remove -b or --background-process from sys.argv
+        sys.argv = [arg for arg in sys.argv if arg not in ["-b", "--background-process"]]
 
-        logger.debug(f"Python Command: {command}")
+        logger.debug(f"Command: {' '.join(sys.argv)}")
 
         subprocess.Popen(  # noqa: S603
-            [
-                sys.executable,
-                "-c",
-                command,
-            ],
+            sys.argv,
             stdout=stdout,
             stderr=stderr,
         )
