ccad63e507f645873981619b7cdddc014b5e1b4e
Semjon Geist <semjon.geist@ionos.com>
2023-10-05 11:14:23 +0200

    add eval_json

--- cornflakes/__init__.py
+++ cornflakes/__init__.py
@@ -1,5 +1,5 @@
 """Top Level Module."""  # noqa: RST303 D205
-from _cornflakes import apply_match, eval_csv, eval_datetime, eval_type, extract_between, ini_load
+from _cornflakes import apply_match, eval_csv, eval_datetime, eval_type, extract_between, ini_load, eval_json
 from cornflakes.builder import generate_config_group_module
 from cornflakes.common import patch_module
 from cornflakes.logging import attach_log, setup_logging
@@ -7,13 +7,14 @@ from cornflakes.parser import yaml_load
 
 __author__ = "Semjon Geist"
 __email__ = "semjon.geist@ionos.com"
-__version__ = "3.3.24"  # <<FORCE_BUMP>>
+__version__ = "3.3.25"  # <<FORCE_BUMP>>
 
 __all__ = [
     "ini_load",
     "eval_type",
     "eval_datetime",
     "eval_csv",
+    "eval_json",
     "extract_between",
     "apply_match",
     "generate_config_group_module",
