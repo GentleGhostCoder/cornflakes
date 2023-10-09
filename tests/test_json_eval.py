d1b32a48c2ab7e0cacb23d526bdcb2167c2daad6
Semjon Geist <semjon.geist@ionos.com>
2023-10-09 15:11:17 +0200

    fix background-process

--- tests/test_json_eval.py
+++ tests/test_json_eval.py
@@ -1,15 +1,5 @@
 from cornflakes import eval_json
 
-with open("./node_cpu_seconds_total_20230912_153000.json") as f:
-    data = f.read()
-    print(data[1:1000])
-
-
-json_chunk = """
-{"status":"success","data":{"resultType":"matrix","result":[{"metric":{"__name__":"node_cpu_seconds_total","alias":"datacenteraggregation","cluster":"F10C1","cpu":"0","datacenter":"us-mkc-ga-kvm-live","instance":"onode100101.server.lan:9100","job":"clusters","mode":"idle"},"values":[[1694532600,"14179793.82"],[1694532660,"14179793.82"],[1694532720,"14179793.82"],[1694532780,"14179793.82"],[1694532840,"14180040.55"],[1694532900,"14180040.55"]]}]}}
-"""
-eval_json(json_chunk)
-
 sample_json_str = """
     {
         "name": "John",
